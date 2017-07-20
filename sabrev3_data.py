281381e1e3b550d486ce790c1da9199ef342f299
PointyShinyBurning <PointyShinyBurning@gmail.com>
2017-07-20 14:14:25 +0100

    give up, use PythonOperator

--- sabrev3_data.py
+++ sabrev3_data.py
@@ -6,6 +6,7 @@ from airflow.operators.http_operator import SimpleHttpOperator
 from datetime import datetime
 import shutil
 from cpgintegrate.connectors import OpenClinica
+import requests
 
 xml_dump_path = BaseHook.get_connection('temp_file_dir').extra_dejson.get("path")+"openclinica.xml"
 
@@ -24,6 +25,19 @@ def save_form_to_csv(form_oid_prefix, save_path):
     OpenClinica("http://cmp.slms.ucl.ac.uk/OpenClinica", "S_SABREV3_4350")\
         .get_dataset(xml_dump_path, form_oid_prefix)\
         .to_csv(save_path)
+
+
+def push_to_ckan(csv_path, resource_id):
+    conn = BaseHook.get_connection('ckan')
+    file = open(csv_path, 'rb')
+    requests.post(
+        url=conn.host + '/api/3/action/resource_update',
+        data={'upload':file, "id":resource_id},
+        headers=conn.extra_dejson(),
+    )
+
+def post_to_ckan(id, file_path):
+    pass
 
 forms_and_ids = {'F_ANTHROPO': '746f91c8-ab54-4476-95e3-a9da2dafdffc'}
 
@@ -58,13 +72,11 @@ for form_prefix, resource_id in forms_and_ids.items():
     )
     pull_dataset << unzip
 
-    push_dataset = SimpleHttpOperator(
-        endpoint='/api/3/action/resource_update',
-        method='POST',
-        headers={"Content-Type": "multipart/form-data"},
-        data="id=resource_id",
-        http_conn_id='ckan',
+    push_dataset = PythonOperator(
+        python_callable=push_to_ckan,
+        op_args=[csv_path, resource_id],
+        task_id=form_prefix + "_push_to_ckan",
         dag=dag,
-        task_id=form_prefix + "_push",
     )
+
     push_dataset << pull_dataset
