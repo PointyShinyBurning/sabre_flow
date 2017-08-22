bf2a196baf87e37016f10e253e6299443c311fbe
PointyShinyBurning <PointyShinyBurning@gmail.com>
2017-08-22 12:39:09 +0100

    Refactor for arbitrary connectors

--- sabrev3_data.py
+++ sabrev3_data.py
@@ -5,15 +5,11 @@ from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 import shutil
 import cpgintegrate
-from cpgintegrate.connectors import OpenClinica
+from cpgintegrate.connectors import OpenClinica, XNAT
 from cpgintegrate.processors import tanita_bioimpedance, epiq7_liverelast
 import requests
 import logging
 import numpy as np
-
-xml_dump_path = BaseHook.get_connection('temp_file_dir').extra_dejson.get("path")+"openclinica.xml"
-openclinica_conn = BaseHook.get_connection('openclinica')
-openclinica_auth = openclinica_conn.login, openclinica_conn.password
 
 
 def unzip_first_file(zip_path, destination):
@@ -26,19 +22,20 @@ def unzip_first_file(zip_path, destination):
     destination_file.close()
 
 
-def save_form_to_csv(form_oid_prefix, save_path, **context):
-    OpenClinica(openclinica_conn.host, "S_SABREV3_4350", xml_path=xml_dump_path)\
-        .get_dataset(form_oid_prefix)\
+def process_files_and_save(save_path, connector=None, connector_args=None, iter_files_args=None,
+                           processor=None, processor_args=None, cols=None, **context):
+    connector_instance = connector(**connector_args)
+    processor_instance = processor(**processor_args) if processor_args else processor
+    (cpgintegrate
+     .process_files(connector_instance.iter_files(iter_files_args), processor_instance)
+     .loc[:, cols if cols else ":"]
+     .to_csv(save_path))
+
+
+def save_dataset(save_path, connector=None, connector_args=None, dataset_args=None, **context):
+    connector(**connector_args)\
+        .get_dataset(dataset_args)\
         .to_csv(save_path)
-
-
-def save_processed_files_to_csv(item_oid, save_path, processor=None, cols=None, **context):
-
-    logging.info("Logging into OpenClinica")
-    oc = OpenClinica(openclinica_conn.host, "S_SABREV3_4350", xml_path=xml_dump_path, auth=openclinica_auth)
-
-    df = cpgintegrate.process_files(oc.iter_files(item_oid), processor)
-    df.to_csv(save_path)
 
 
 def push_to_ckan(push_csv_path, push_resource_id):
@@ -54,12 +51,23 @@ def push_to_ckan(push_csv_path, push_resource_id):
     assert res.status_code == 200
 
 
-forms_and_ids = {'F_ANTHROPO': (save_form_to_csv, '40aa2125-2132-473b-9a06-302ed97060a6', {}),
-                 'I_ANTHR_BIOIMPEDANCEFILE': (save_processed_files_to_csv, 'f1755dba-b898-4af4-bb4e-0c7977ef8a37',
-                                              {'cols': ['BMI_WEIGHT', 'BODYFAT_FATM', 'BODYFAT_FATP'],
-                                               'processor': tanita_bioimpedance.to_frame}),
-                 'I_LIVER_ELASTOGRAPHYFILE': (save_processed_files_to_csv, 'e751379f-2a2b-472c-b454-05cf83d8f099',
-                                              {'processor': epiq7_liverelast.to_frame}),
+xml_dump_path = BaseHook.get_connection('temp_file_dir').extra_dejson.get("path")+"openclinica.xml"
+openclinica_conn = BaseHook.get_connection('openclinica')
+openclinica_conn_args = (openclinica_conn.host, "S_SABREV3_4350", xml_dump_path, (openclinica_conn.login, openclinica_conn.password))
+
+xnat_conn = BaseHook.get_connection('xnat')
+xnat_conn_args = (xnat_conn.host, "SABREv3", (xnat_conn.login, xnat_conn.password))
+
+
+forms_and_ids = {'F_ANTHROPO': ('40aa2125-2132-473b-9a06-302ed97060a6',save_dataset,
+                                [OpenClinica, openclinica_conn_args, ['F_ANTHROPO']]),
+                 'I_ANTHR_BIOIMPEDANCEFILE': ('f1755dba-b898-4af4-bb4e-0c7977ef8a37', process_files_and_save,
+                                              [OpenClinica, openclinica_conn_args,
+                                               'I_ANTHR_BIOIMPEDANCEFILE', tanita_bioimpedance,
+                                                   None, ['BMI_WEIGHT', 'BODYFAT_FATM', 'BODYFAT_FATP']]),
+                 'I_LIVER_ELASTOGRAPHYFILE': ('e751379f-2a2b-472c-b454-05cf83d8f099', process_files_and_save,
+                                              [OpenClinica, openclinica_conn_args,
+                                               'I_LIVER_ELASTOGRAPHYFILE', epiq7_liverelast]),
                  }
 
 default_args = {
@@ -84,19 +92,23 @@ unzip = PythonOperator(
 
 previous = unzip
 
-for form_prefix, (callee, resource_id, extra_args) in forms_and_ids.items():
-    csv_path = BaseHook.get_connection('temp_file_dir').extra_dejson.get("path")+form_prefix+"_t3.csv"
+for resource_name_root, (ckan_resource_id, callee, callee_args) in forms_and_ids.items():
+    csv_path = BaseHook.get_connection('temp_file_dir').extra_dejson.get("path")+resource_name_root+"_t3.csv"
     pull_dataset = PythonOperator(
-        python_callable=callee, op_args=[form_prefix, csv_path], op_kwargs=extra_args,
-        task_id=form_prefix+"_export", dag=dag, provide_context=True
+        python_callable=callee, op_args=[csv_path]+callee_args,
+        task_id=resource_name_root+"_export", dag=dag, provide_context=True
     )
     pull_dataset << previous
 
     push_dataset = PythonOperator(
-        python_callable=push_to_ckan, op_args=[csv_path, resource_id],
-        task_id=form_prefix + "_push_to_ckan", dag=dag,
+        python_callable=push_to_ckan, op_args=[csv_path, ckan_resource_id],
+        task_id=resource_name_root + "_push_to_ckan", dag=dag,
     )
-    # Run the OpenClinica extracts sequentially because its session management is stoopid
-    push_dataset << pull_dataset
-    previous = pull_dataset
 
+    push_dataset << pull_dataset
+
+    # Run the OpenClinica extracts sequentially because its session management is stoopid
+    if callee_args[0] == OpenClinica:
+        previous = pull_dataset
+    else:
+        previous = unzip
