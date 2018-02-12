2b1ed1d14677f0fbb212b40b5bca181381e7e2cf
PointyShinyBurning <PointyShinyBurning@gmail.com>
2018-02-12 16:18:00 +0000

    Replace requirements with setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="sabrev3_flow_flow",
+    name="sabrev3_flow",
     install_requires=[
         'cpgintegrate>=0.2.12',
         'apache-airflow>=1.9.0',
