ab8b35f6076d856f9ac8245718f204a280e32cd4
PointyShinyBurning <pointyshinyburning@gmail.com>
2018-12-03 12:19:53 +0000

    Bump cpgintegrate version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="sabrev3_flow",
     install_requires=[
-        'cpgintegrate>=0.2.14',
+        'cpgintegrate>=0.2.16',
         'apache-airflow>=1.9.0',
     ],
 )
